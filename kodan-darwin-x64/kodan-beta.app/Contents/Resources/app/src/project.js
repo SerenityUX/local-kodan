454f87cb827cc2a6b290e9850b547ef12262f29b
SerenityUX <thomas@serenidad.app>
2024-09-13 16:52:47 +0900

    ok

--- kodan-darwin-x64/kodan-beta.app/Contents/Resources/app/src/project.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import ProjectComponent from "./components/ProjectComponent";
-
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<ProjectComponent />);
